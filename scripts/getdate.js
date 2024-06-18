006c943ede2a3b92b7c338a3261b26f2371dd581
erick montoya <byakyuya250kushki@gmail.com>
2024-06-17 23:02:18 -0600

    first commit

---
+++ scripts/getdate.js
@@ -0,0 +1,5 @@
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
