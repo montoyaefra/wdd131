92aef3a60dfb9c5e5c0250b291d002ee41b10ba5
erick montoya <byakyuya250kushki@gmail.com>
2024-07-07 22:17:46 -0600

    js added

---
+++ scripts/weather.js
@@ -0,0 +1,7 @@
+
+let currentTemperature = 10; 
+let currentWindSpeed = 5;    
+
+let windChill = calculateWindChill(currentTemperature, currentWindSpeed);
+
+console.log(`Wind Chill Factor: ${windChill}`);
